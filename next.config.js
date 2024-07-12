95f900ce0269c23f078d8e81faba8cd6dd754c75
MJ Zorick <maryjane.zorick@gmail.com>
2024-07-11 19:35:50 -0700

    revert cache-handler

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ const { withAtlasConfig } = require("@wpengine/atlas-next")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
-    isrMemoryCacheSize: 0,
-  },
   reactStrictMode: true,
   images: {
     domains: ["atlasnextisr.wpenginepowered.com"],
