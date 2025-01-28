922c300f43c99660b07e2dc86b4064ddc26fdf3d
MJ Zorick <maryjane.zorick@gmail.com>
2025-01-28 09:00:42 -0800

    added redirects

--- next.config.js
+++ next.config.js
@@ -3,9 +3,19 @@ const { withAtlasConfig } = require("@wpengine/atlas-next")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     domains: ["atlasnextisr.wpenginepowered.com"],
   },
 };
 
-module.exports = withAtlasConfig(nextConfig)
+module.exports = withAtlasConfig(nextConfig);
+
